b42022aa55346bb72d89bff7dc0d5891cc1f2a89
bassel rida <bslri@bassel.CODCI.COM>
2025-12-27 08:57:32 +0000

    ff

--- src/types/stock.types.ts
+++ src/types/stock.types.ts
@@ -3,11 +3,10 @@ export interface stocks {
   article_reference: string;
   article_designation: string;
   article_famille: string;
-  article_cmup : number;
-  article_prix_ttc: number;
   article_quantity: number;
-  total_cmup: number; //CMUP
-  total_ttc: number;
+  depot_intitule: string;
+  depot_code: string;
+  famille_code: string;
 }
 
 export interface rotation_stock {
